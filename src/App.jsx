c27033ee8b4d5ba537502aefd98e655ece56291d
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-06-23 19:19:33 +0600

    Deploy Rest Countries To Surge Or Netlify

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Countries from "./Components/Countries/Countries";
 function App() {
 	return (
 		<>
-			<h1>React World Tour</h1>
+			<h1>React Awesome World Tour</h1>
             <Countries></Countries>
 		</>
 	);
